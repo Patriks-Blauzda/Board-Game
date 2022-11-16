182e6e84a1d500c738d667bf9ee36332fcecdc7a
Patriks <pblauzda@gmail.com>
2022-11-16 16:32:34 +0200

    first commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Player player1 = new Player();
+
+        System.out.println(player1.roll(2));
     }
 }
